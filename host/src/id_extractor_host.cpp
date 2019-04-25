4794c00887a46cb5eaabbbbbf4a547f8b4eb444c
bmorcos <morcos.ben@gmail.com>
2019-04-25 11:17:43 -0400

    Add compiled library host

--- host/src/id_extractor_host.cpp
+++ host/src/id_extractor_host.cpp
@@ -16,120 +16,120 @@ Ben Morcos
 #include "CL/opencl.h"
 #include "AOCLUtils/aocl_utils.h"
 
-namespace aocl_utils{
+namespace aocl_utils {
 
-// OpenCL Setup
-cl_platform_id platform = NULL;
-scoped_array<cl_device_id> device;
-unsigned n_devices = 0;
-cl_context context;
-cl_command_queue queue;
-cl_program program = NULL;
-cl_kernel kernel;
-cl_mem chip_id_buf;
+  // OpenCL Setup
+  cl_platform_id platform = NULL;
+  scoped_array<cl_device_id> device;
+  unsigned n_devices = 0;
+  cl_context context;
+  cl_command_queue queue;
+  cl_program program = NULL;
+  cl_kernel kernel;
+  cl_mem chip_id_buf;
 
-// Data pointer for passing data between host/device
-typedef unsigned long long uint64;
-uint64 chip_id_host;  // 64b
+  // Data pointer for passing data between host/device
+  typedef unsigned long long uint64;
+  uint64 chip_id_host;  // 64b
 
-extern "C" uint64 get_id() {
-  cl_int status;
-  cl_event kernel_event;
+  extern "C" uint64 get_id() {
+    cl_int status;
+    cl_event kernel_event;
 
-  // Set kernel arg
-  unsigned argi = 0;
-  status = clSetKernelArg(kernel, argi++, sizeof(cl_mem), &chip_id_buf);
-  checkError(status, "Failed to set argument %d", argi - 1);
+    // Set kernel arg
+    unsigned argi = 0;
+    status = clSetKernelArg(kernel, argi++, sizeof(cl_mem), &chip_id_buf);
+    checkError(status, "Failed to set argument %d", argi - 1);
 
-  // Run
-  printf("Launching kernel... \n\r");
-  status = clEnqueueTask(queue, kernel, 0, NULL, &kernel_event);
-  checkError(status, "Failed to launch kernel");
+    // Run
+    printf("Launching kernel... \n\r");
+    status = clEnqueueTask(queue, kernel, 0, NULL, &kernel_event);
+    checkError(status, "Failed to launch kernel");
 
-  clWaitForEvents(cl_uint(1), &kernel_event);  // wait for kernels to end
-  clReleaseEvent(kernel_event);
+    clWaitForEvents(cl_uint(1), &kernel_event);  // wait for kernels to end
+    clReleaseEvent(kernel_event);
 
-  // Read output
-  printf("Reading ID from buffer...\n");
-  status = clEnqueueReadBuffer(
-    queue, chip_id_buf, CL_TRUE, 0, 8, &chip_id_host, 0, 0, 0);
-  checkError(status, "Failed to chip ID from buffer");
+    // Read output
+    printf("Reading ID from buffer...\n");
+    status = clEnqueueReadBuffer(
+      queue, chip_id_buf, CL_TRUE, 0, 8, &chip_id_host, 0, 0, 0);
+    checkError(status, "Failed to chip ID from buffer");
 
-  return chip_id_host;
-}
-
-extern "C" bool setup(char* aocx_file) {
-  /*
-
-  `aocx_file` - full path to *.aocx omitting the extension
-      eg. "/home/root/single_pes"
-  */
-  cl_int status;
-
-  printf("AOCX FILE: %s\n", aocx_file);
-
-  /*
-   * Setup all the OpenCL objects needed
-   */
-
-  // Get the OpenCL platform.
-  platform = findPlatform("Altera");
-  if (platform == NULL) {
-      printf("ERROR: Unable to find Altera OpenCL platform.\n");
-      return false;
+    return chip_id_host;
   }
 
-  // Get device
-  device.reset(getDevices(platform, CL_DEVICE_TYPE_ALL, &n_devices));
+  extern "C" bool setup(char* aocx_file) {
+    /*
 
-  // Get context
-  context = clCreateContext(
-      NULL, 1, device, &oclContextCallback, NULL, &status);
-  checkError(status, "Failed to create context");
+    `aocx_file` - full path to *.aocx omitting the extension
+        eg. "/home/root/id_extractor"
+    */
+    cl_int status;
 
-  // Create program
-  std::string binary_file = getBoardBinaryFile(aocx_file, device[0]);
-  program = createProgramFromBinary(context, binary_file.c_str(), device, 1);
+    printf("AOCX FILE: %s\n", aocx_file);
 
-  // Build the program
-  status = clBuildProgram(
-    program, 0, NULL, NULL, NULL, NULL);
-  checkError(status, "Failed to build program");
+    /*
+     * Setup all the OpenCL objects needed
+     */
 
-  // Create OpenCL objects
-  queue = clCreateCommandQueue(
-    context, device[0], CL_QUEUE_PROFILING_ENABLE, &status);
-  checkError(status, "Failed to create command queue");
+    // Get the OpenCL platform.
+    platform = findPlatform("Altera");
+    if (platform == NULL) {
+        printf("ERROR: Unable to find Altera OpenCL platform.\n");
+        return false;
+    }
 
-  kernel = clCreateKernel(program, "id_extractor", &status);
-  checkError(status, "Failed to create kernel");
+    // Get device
+    device.reset(getDevices(platform, CL_DEVICE_TYPE_ALL, &n_devices));
 
-  // Create buffer for reading
-  chip_id_buf = clCreateBuffer(
-      context, CL_MEM_WRITE_ONLY, 8, NULL, &status);
-  checkError(status, "Failed to create buffer for chip_id");
+    // Get context
+    context = clCreateContext(
+        NULL, 1, device, &oclContextCallback, NULL, &status);
+    checkError(status, "Failed to create context");
 
-  return true;
-}
+    // Create program
+    std::string binary_file = getBoardBinaryFile(aocx_file, device[0]);
+    program = createProgramFromBinary(context, binary_file.c_str(), device, 1);
 
-extern "C" void cleanup() {
-  /*
-  Release all memory objects
-  */
-  if (kernel) {
-      clReleaseKernel(kernel);
+    // Build the program
+    status = clBuildProgram(
+      program, 0, NULL, NULL, NULL, NULL);
+    checkError(status, "Failed to build program");
+
+    // Create OpenCL objects
+    queue = clCreateCommandQueue(
+      context, device[0], CL_QUEUE_PROFILING_ENABLE, &status);
+    checkError(status, "Failed to create command queue");
+
+    kernel = clCreateKernel(program, "id_extractor", &status);
+    checkError(status, "Failed to create kernel");
+
+    // Create buffer for reading
+    chip_id_buf = clCreateBuffer(
+        context, CL_MEM_WRITE_ONLY, 8, NULL, &status);
+    checkError(status, "Failed to create buffer for chip_id");
+
+    return true;
   }
-  if (queue) {
-      clReleaseCommandQueue(queue);
+
+  extern "C" void cleanup() {
+    /*
+    Release all memory objects
+    */
+    if (kernel) {
+        clReleaseKernel(kernel);
+    }
+    if (queue) {
+        clReleaseCommandQueue(queue);
+    }
+    if (chip_id_buf) {
+        clReleaseMemObject(chip_id_buf);
+    }
+    if (program) {
+        clReleaseProgram(program);
+    }
+    if (context) {
+        clReleaseContext(context);
+    }
   }
-  if (chip_id_buf) {
-      clReleaseMemObject(chip_id_buf);
-  }
-  if (program) {
-      clReleaseProgram(program);
-  }
-  if (context) {
-      clReleaseContext(context);
-  }
-}
-} // namespace
+}  // namespace aocl_utils
